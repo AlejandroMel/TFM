a54d43d1788d38ed5ede68ed4f1190a0805a9783
AlejandroMel <89985040+AlejandroMel@users.noreply.github.com>
2022-06-27 15:42:43 +0200

    Add files via upload

--- apollo.bash
+++ apollo.bash
@@ -1,32 +1,18 @@
-name=$1
+sequence=$1
+modelfolder=$2
 
-mkdir -p models/$name
+name=${sequence%.*}
 
-(
-  ls cabs*/${name}/*/output_pdbs/model*.pdb ;\
-  ls dncon2/${name}/out/confold1/stage2/*model*.pdb ; \
-  ls dncon2/${name}/out/confold2-*${name}*.dncon2/top-models/top-4*.pdb ; \
-  ls dncon2/${name}/out/unicon3d_models/*.pdb ; \
-  ls i-tasser/${name}/model*.pdb ; \
-  ls trRosetta/${name}/model*.pdb \
-) | while read mod ; do 
-    dest=models/${name}/`echo $mod | tr '/' '-'`
-    #echo cp $mod $dest
-    if [ ! -e $dest ] ; then
-        cp $mod $dest
-    fi
-done
+mkdir -p $modelfolder
 
-cd models
-cp ../seq/${name}.fasta .
-ls -1 ${name}/* > ${name}.mlist
+ls -1 ${modelfolder}/* > ${name}.mlist	# mlist is the model list
 mkdir -p ${name}.apollo
 
 ~/contrib/multicom_toolbox/apollo_pairwise.64bit/pairwise_qa_apollo \
     ${name}.mlist \
-    ${name}.fasta \
+    ${sequence} \
     ~/contrib/multicom_toolbox/apollo_pairwise.64bit/tm_score/TMscore_64 \
     ${name}.apollo \
-    ${name}
+    ${modelfolder}
 
 cd ..
